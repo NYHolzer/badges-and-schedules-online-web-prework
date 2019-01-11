9d2c0100a825cfe25a16066813ecb78cf639f226
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-11 00:58:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms (attendees) 
   attendees.map.with_index(1) do |name, index|
-  "Hello #{name}! You'll be assigned to room #{index}"
+  "Hello, #{name}! You'll be assigned to room #{index}"
   end
 end
