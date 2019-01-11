370050a2892ee78f55195dae35cbb127be565a2f
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-11 00:37:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+def badge_maker (name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+  names.map do |name|
+     badge_maker (name)
+  end
+end 
+
+def assign_rooms (attendees)
+  room_assignment = []
+  attendees.each_with_index |name, index|
+  room_assignment << "Hello #{name}! You'll be assigned to room #{index}"
+  end
+end
